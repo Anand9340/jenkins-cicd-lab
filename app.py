40c0a49ca073fe07afbb7eef8f9995e735e91a00
Anand9340 <anandsvn2001.com>
2026-09-16 16:39:20 +0000

    Update application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins CI/CD!"
+    return "Hello from Jenkins CI/CD - Version 2!"
 
 @app.route("/health")
 def health():
